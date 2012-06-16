ff1cb865824dafd32eb3b7a71b6643f3269c61dc
Jochen Klar <jochenklar@googlemail.com>
2012-06-16 13:27:22 +0200

    gpl files and file headers

--- source/cie.f90
+++ source/cie.f90
@@ -1,3 +1,28 @@
+!########################################################################!
+!#                                                                      #!
+!# cie.f90                                                              #!
+!# Copyright (C) 2007-2012 Jochen Klar                                  #!
+!#                                                                      #!
+!# This file is part of evora - a hydrodynamic code for the computation #!
+!# of the cosmological evolution of a polytropic fluid including the    #!
+!# influence of gravity, primordial chemical processes, radiative       #!
+!# cooling, heating by a UV background, and thermal conduction.         #!
+!#                                                                      #!
+!# evora is free software: you can redistribute it and/or modify        #!
+!# it under the terms of the GNU General Public License as published by #!
+!# the Free Software Foundation, either version 3 of the License, or    #!
+!# (at your option) any later version.                                  #!
+!#                                                                      #!
+!# evora is distributed in the hope that it will be useful,             #!
+!# but WITHOUT ANY WARRANTY; without even the implied warranty of       #!
+!# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #!
+!# GNU General Public License for more details.                         #!
+!#                                                                      #!
+!# You should have received a copy of the GNU General Public License    #!
+!# along with evora.  If not, see <http://www.gnu.org/licenses/>.       #!  
+!#                                                                      #!
+!########################################################################!
+
 subroutine cie(rho,press,n)
   use global
   implicit none
